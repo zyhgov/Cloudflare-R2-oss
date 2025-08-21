604272ca76c38894bcbfb6312b9158ff1fdad790
杖雍皓 <121595742+zyhgov@users.noreply.github.com>
2025-08-21 10:03:18 +0800

    Add files via upload

--- assets/App.vue
+++ assets/App.vue
@@ -13,7 +13,7 @@
     <button class="upload-button circle" @click="showUploadPopup = true">
       <img
         style="filter: invert(100%)"
-        src="https://cdnjs.cloudflare.com/ajax/libs/material-design-icons/4.0.0/png/file/upload_file/materialicons/36dp/2x/baseline_upload_file_black_36dp.png"
+        src="./上传.png"
         alt="Upload"
         width="36"
         height="36"
@@ -55,7 +55,7 @@
         >
           <div class="file-icon">
             <img
-              src="https://cdnjs.cloudflare.com/ajax/libs/material-design-icons/4.0.0/png/file/folder/materialicons/36dp/2x/baseline_folder_black_36dp.png"
+              src="./文件夹.png"
               width="36"
               height="36"
               alt="Folder"
@@ -76,7 +76,7 @@
         >
           <div class="file-icon">
             <img
-              src="https://cdnjs.cloudflare.com/ajax/libs/material-design-icons/4.0.0/png/file/folder/materialicons/36dp/2x/baseline_folder_black_36dp.png"
+              src="./文件夹.png"
               width="36"
               height="36"
               alt="Folder"
